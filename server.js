ac3c06871850f6fd98421164172a0c12a043f739
HeideQueen <lucyfabz@gmail.com>
2019-11-11 02:32:51 -0600

    add profile and image routes

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ app.post('/signin', (req, res) => {
 	if (req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password) {
 		res.json('success');
 	} else {
-		res.status(400).json('error logging in');
+		res.status(404).json('error logging in');
 	}
 });
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
 	dataBase.users.push({
-		id: '124',
+		id: '125',
 		name: name,
 		email: email,
 		password: password,
@@ -48,6 +48,23 @@ app.post('/register', (req, res) => {
 		joined: new Date()
 	});
 	res.json(dataBase.users[dataBase.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	const [ found ] = dataBase.users.filter((user) => user.id === id);
+	!found ? res.status(404).json('user not found') : res.json(found);
+});
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	const [ found ] = dataBase.users.filter((user) => user.id === id);
+	if (found) {
+		found.entries++;
+		res.json(found.entries);
+	} else {
+		res.status(404).json('user not found');
+	}
 });
 
 app.listen(3000, () => console.log("server's up nya!!"));
